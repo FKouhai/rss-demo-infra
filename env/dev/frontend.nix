cf2cc5c327b17db72c57ddbcebe76b7e70515a4b
FKouhai <frandres00@gmail.com>
2025-09-16 00:14:06 +0200

    chore: bump frontend

--- env/dev/frontend.nix
+++ env/dev/frontend.nix
@@ -22,7 +22,7 @@
             spec = {
               securityContext.fsGroup = 1000;
               containers.frontend = {
-                image = "ghcr.io/fkouhai/rss_frontend-x86_64-linux:0.0.7";
+                image = "ghcr.io/fkouhai/rss_frontend-x86_64-linux:0.0.8";
                 imagePullPolicy = "IfNotPresent";
                 env = [
                   {
