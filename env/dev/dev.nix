2fba521d5c0e3e15fc8c1eafedb1e614caf6a1e7
FKouhai <frandres00@gmail.com>
2025-10-31 21:58:21 +0100

    feat: enable autoSync

--- env/dev/dev.nix
+++ env/dev/dev.nix
@@ -2,4 +2,11 @@
   nixidy.target.repository = "https://github.com/FKouhai/rss-demo-infra.git";
   nixidy.target.branch = "main";
   nixidy.target.rootPath = "./manifests/dev";
+  nixidy.defaults.syncPolicy = {
+    autoSync = {
+      enable = true;
+      selfHeal = true;
+      prune = true;
+    };
+  };
 }
