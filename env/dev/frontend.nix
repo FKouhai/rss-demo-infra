e5a267b2753b63752a6f3c797f3ef41c69d39007
FKouhai <frandres00@gmail.com>
2025-10-31 18:45:08 +0100

    feat: bump images

--- env/dev/frontend.nix
+++ env/dev/frontend.nix
@@ -22,7 +22,7 @@
             spec = {
               securityContext.fsGroup = 1000;
               containers.frontend = {
-                image = "ghcr.io/fkouhai/rss_frontend-x86_64-linux:0.1.6";
+                image = "ghcr.io/fkouhai/rss_frontend-x86_64-linux:0.1.7";
                 imagePullPolicy = "IfNotPresent";
                 env = [
                   {
