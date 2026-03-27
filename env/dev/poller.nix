d72aa076d1d10afb4583eb9efeb07778e7d18a3e
FKouhai <frandres00@gmail.com>
2026-03-27 06:02:37 +0100

    chore(poller): bump to 1.0.2 for hotfix

--- env/dev/poller.nix
+++ env/dev/poller.nix
@@ -39,7 +39,7 @@
                 }
               ];
               containers.poller = {
-                image = "ghcr.io/fkouhai/rss_poller-x86_64-linux:1.0.1";
+                image = "ghcr.io/fkouhai/rss_poller-x86_64-linux:1.0.2";
                 imagePullPolicy = "IfNotPresent";
                 livenessProbe = {
                   httpGet = {
