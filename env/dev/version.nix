e6e60f139411ea51929a0db7a0f60a07c596d2b3
FKouhai <frandres00@gmail.com>
2026-04-19 16:01:06 +0200

    chore: uplift to v1.0.5

--- env/dev/version.nix
+++ env/dev/version.nix
@@ -2,7 +2,7 @@
 {
   options.demo.version = lib.mkOption {
     type = lib.types.str;
-    default = "1.0.4";
+    default = "1.0.5";
     description = "Version tag used for all demo application images and OTEL resource attributes.";
   };
 }
